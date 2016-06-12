872ae2ce0eeaa7a6a6e2bb9a1fd9b88ccd767af1
Sheldon <sheldonlau1@gmail.com>
2016-06-11 19:54:45 -0700

    wrote indexPage and size methods

--- javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
@@ -36,8 +36,11 @@ public class TermCounter {
 	 * @return
 	 */
 	public int size() {
-        // TODO: fill this in.
-		return -1;
+    int count = 0;
+    for(String key : map.keySet()) {
+      count += map.get(key);
+    }
+		return count;
 	}
 
 	/**
